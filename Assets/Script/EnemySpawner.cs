a7d5557d080b67f462f9042da8fab41489075ec8
6epreu <6epreu@gmail.com>
2016-07-17 14:56:42 +0300

    Added bunny jump, cactus generator, cactus destroyer

---
+++ Assets/Script/EnemySpawner.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour {
+
+    public float nextSpawnTime = 0;
+    public float spawnTimeFrequency = 1;
+    public float randomDelay = 1;
+    public GameObject enemyPrefab;
+
+    // Use this for initialization
+    void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Time.time > nextSpawnTime)
+        {   
+            Instantiate(enemyPrefab, transform.position, Quaternion.identity);
+            nextSpawnTime = Time.time + spawnTimeFrequency +  Random.Range(0, randomDelay);
+            Debug.Log("nextSpawnTime = " + nextSpawnTime);
+        }
+	}
+}
--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -2,29 +2,35 @@
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
-
+    
     private Rigidbody2D mRigidBody;
-    public float playerUpForce = 500f;
     private Animator mAnimator;
+    public float force = 500;
+    
     
     // Use this for initialization
 	void Start () {
         mRigidBody = GetComponent<Rigidbody2D>();
         mAnimator = GetComponent<Animator>();
-
-        
-
     }
 	
 	// Update is called once per frame
 	void Update () {
-        
 
         if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
         {
-            mRigidBody.AddForce(transform.up * playerUpForce);
+            mRigidBody.AddForce(Vector2.up * force);
         }
-        
-        mAnimator.SetFloat("vVelocity", Mathf.Abs(mRigidBody.velocity.y));
+
+        mAnimator.SetFloat("vVelocity", mRigidBody.velocity.y);
+    }
+
+    void OnCollisionEnter2D( Collision2D collision )
+    {
+        if (collision.collider.gameObject.layer == LayerMask.NameToLayer("Enemy"))
+        {
+            Debug.Log("asdf");
+            Application.LoadLevel(Application.loadedLevel);
+        }
     }
 }
