0d41105cbdd5118d91da49052a1476a4c1ab5f86
6epreu <6epreu@gmail.com>
2016-07-17 17:07:29 +0300

    added score

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
@@ -6,31 +7,60 @@ public class PlayerController : MonoBehaviour {
     private Rigidbody2D mRigidBody;
     private Animator mAnimator;
     public float force = 500;
-    
-    
+    private float playerHurtTime = -1;
+    private BoxCollider2D mCollider;
+    public Text scoreText;
+
+
     // Use this for initialization
-	void Start () {
+    void Start () {
         mRigidBody = GetComponent<Rigidbody2D>();
         mAnimator = GetComponent<Animator>();
+        mCollider = GetComponent<BoxCollider2D>();
     }
 	
 	// Update is called once per frame
 	void Update () {
-
-        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        
+        if (playerHurtTime == -1)
         {
-            mRigidBody.AddForce(Vector2.up * force);
-        }
+            if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+            {
+                mRigidBody.AddForce(Vector2.up * force);
+            }
 
-        mAnimator.SetFloat("vVelocity", mRigidBody.velocity.y);
+            mAnimator.SetFloat("vVelocity", mRigidBody.velocity.y);
+            scoreText.text = Time.time.ToString("0.0");
+        }
+        else if ( Time.time > playerHurtTime + 2 ) 
+        {
+            scoreText.text = "0.0";
+            Application.LoadLevel(Application.loadedLevel);
+        }
     }
 
     void OnCollisionEnter2D( Collision2D collision )
     {
         if (collision.collider.gameObject.layer == LayerMask.NameToLayer("Enemy"))
         {
-            Debug.Log("asdf");
-            Application.LoadLevel(Application.loadedLevel);
+            Debug.Log("hurt");
+            
+            mAnimator.SetBool("bHurt", true);
+            playerHurtTime = Time.time;
+
+            // остаенавливаем существующий кактусы!
+            foreach (MoveLeft ml in FindObjectsOfType<MoveLeft>())
+            {
+                ml.enabled = false;
+            }
+
+            // остаенавливаем фабрику кактусов
+            FindObjectOfType<EnemySpawner>().enabled = false;
+
+            mRigidBody.velocity = Vector2.zero;
+            mRigidBody.AddForce(Vector2.up * force);
+            mCollider.enabled = false;
         }
     }
 }
+
