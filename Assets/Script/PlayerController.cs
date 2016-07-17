aabad6cc5b71f41001ed1b78ea0db05bb0d74eee
6epreu <6epreu@gmail.com>
2016-07-17 12:42:27 +0300

    Added level1 with jump game

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody2D mRigidBody;
+    public float playerUpForce = 500f;
+    private Animator mAnimator;
+    
+    // Use this for initialization
+	void Start () {
+        mRigidBody = GetComponent<Rigidbody2D>();
+        mAnimator = GetComponent<Animator>();
+
+        
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            mRigidBody.AddForce(transform.up * playerUpForce);
+        }
+        
+        mAnimator.SetFloat("vVelocity", Mathf.Abs(mRigidBody.velocity.y));
+    }
+}
