4bafa0fc6b8db43d6e94d46d3b4fd787d69ac311
Artem Gorev <artgorev@gmail.com>
2016-08-06 21:34:09 +0300

    leaderboards

---
+++ Assets/LeaderBoardController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class LeaderBoardController : MonoBehaviour
+{
+	private GameObject panel;
+
+	void addRow ()
+	{
+		GameObject tableId = GameObject.Find ("table_id");
+		GameObject rowId = GameObject.Instantiate (tableId);
+//		Vector3 temp = new Vector3 (0, size.y, 0);
+//		rowId.transform.position += temp;
+	}
+
+	// Use this for initialization
+	void Start ()
+	{
+		panel = GameObject.Find ("Panel");
+
+		addRow ();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
